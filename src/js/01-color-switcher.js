29a7b30089f8736330a817a65bda1dd16b99d25d
DramonBuster <terradestroyman@gmail.com>
2022-05-24 15:28:36 +0300

    Завдання 1 виконане

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,44 @@
-import '../css/common.css';
+// import '../css/common.css';
+
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+
+// body.style.backgroundColor = "#000000";
+
+stopButton.disabled = true;
+
+let timeOfChange = null;
+let isActive = false;
+
+const getRandomHexColor = () => `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const setBodyColor = () => body.style.backgroundColor = getRandomHexColor();
+
+// console.log(getRandomHexColor());
+
+const start = () => {
+    if (isActive) {
+        return
+    }
+
+    startButton.disabled = true;
+    stopButton.disabled = false;
+
+    isActive = true;
+    timeOfChange = setInterval(() => setBodyColor(), 1000);
+}
+
+const stop = () => {
+    if (!isActive) {
+        return
+    }
+
+    startButton.disabled = false;
+    stopButton.disabled = true;
+
+    isActive = false;
+    clearInterval(timeOfChange);
+}
+
+startButton.addEventListener('click', start);
+stopButton.addEventListener('click', stop);
